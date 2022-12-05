3e24552ef3a1330b1bb8111f5905d2882fb475e0
kelvin <kelvinsouza369@outlook.com>
2022-12-05 17:16:32 -0300

    🔧 configuration: prettier config

--- apps/next/app/page.tsx
+++ apps/next/app/page.tsx
@@ -1,58 +1,69 @@
-import Image from "next/image";
+import Image from 'next/image'
 
-import styles from "./page.module.css";
+import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Welcome to <a href="https://nextjs.org">Next.js 13!</a>
+                </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+                <p className={styles.description}>
+                    Get started by editing{' '}
+                    <code className={styles.code}>app/page.tsx</code>
+                </p>
 
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://beta.nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
+                <div className={styles.grid}>
+                    <a
+                        className={styles.card}
+                        href="https://beta.nextjs.org/docs"
+                    >
+                        <h2>Documentation &rarr;</h2>
+                        <p>Find in-depth information about Next.js 13</p>
+                    </a>
 
-          <a
-            className={styles.card}
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
+                    <a
+                        className={styles.card}
+                        href="https://github.com/vercel/next.js/tree/canary/examples"
+                    >
+                        <h2>Examples &rarr;</h2>
+                        <p>Explore the Next.js 13 playground.</p>
+                    </a>
 
-          <a
-            className={styles.card}
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+                    <a
+                        className={styles.card}
+                        href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    >
+                        <h2>Deploy &rarr;</h2>
+                        <p>
+                            Deploy your Next.js site to a public URL with
+                            Vercel.
+                        </p>
+                    </a>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
+                    Powered by{' '}
+                    <span className={styles.logo}>
+                        <Image
+                            alt="Vercel Logo"
+                            height={16}
+                            src="/vercel.svg"
+                            width={72}
+                        />
+                    </span>
+                </a>
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image alt="Vercel Logo" height={16} src="/vercel.svg" width={72} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  );
+    )
 }
